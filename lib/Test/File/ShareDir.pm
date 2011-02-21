095999f6d53e441857f1383d002dd8b39d62d194
Kent Fredric <kentfredric@gmail.com>
2011-02-22 01:33:05 +1300

    Build results of 118fc2a (on master)

--- lib/Test/File/ShareDir.pm
+++ lib/Test/File/ShareDir.pm
@@ -2,35 +2,13 @@ use strict;
 use warnings;
 
 package Test::File::ShareDir;
+BEGIN {
+  $Test::File::ShareDir::VERSION = '0.1.0';
+}
 
 # ABSTRACT: Create a Fake ShareDir for your modules for testing.
 
-=head1 SYNOPSIS
 
-    use Test::More;
-
-    use FindBin;
-
-    use Test::File::ShareDir
-        -root => "$FindBin::Bin/../",
-        -share => {
-            -module => { 'My::Module' => 'share/MyModule' }
-        };
-
-    use My::Module;
-
-    use File::ShareDir qw( module_dir );
-
-    module_dir( 'My::Module' ) # dir with files from $dist/share/MyModule
-
-=cut
-
-=head1 DESCRIPTION
-
-At present, this module only has support for creating test-worth 'module' sharedirs, and then
-these are 'new' style sharedirs and are NOT compatible with old File::ShareDirs.
-
-=cut
 
 use File::Temp qw( tempdir );
 use Path::Class qw( dir );
@@ -83,3 +61,51 @@ sub import {
 
 
 1;
+
+__END__
+=pod
+
+=head1 NAME
+
+Test::File::ShareDir - Create a Fake ShareDir for your modules for testing.
+
+=head1 VERSION
+
+version 0.1.0
+
+=head1 SYNOPSIS
+
+    use Test::More;
+
+    use FindBin;
+
+    use Test::File::ShareDir
+        -root => "$FindBin::Bin/../",
+        -share => {
+            -module => { 'My::Module' => 'share/MyModule' }
+        };
+
+    use My::Module;
+
+    use File::ShareDir qw( module_dir );
+
+    module_dir( 'My::Module' ) # dir with files from $dist/share/MyModule
+
+=head1 DESCRIPTION
+
+At present, this module only has support for creating test-worth 'module' sharedirs, and then
+these are 'new' style sharedirs and are NOT compatible with old File::ShareDirs.
+
+=head1 AUTHOR
+
+Kent Fredric <kentnl@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Kent Fredric <kentnl@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
