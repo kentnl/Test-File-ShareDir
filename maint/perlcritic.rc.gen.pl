558f9eb97eb9d4e40eee28e5c7f245c338116296
Kent Fredric <kentfredric@gmail.com>
2014-06-08 02:52:32 +1200

    -RequireUseOfExceptions

--- maint/perlcritic.rc.gen.pl
+++ maint/perlcritic.rc.gen.pl
@@ -12,6 +12,7 @@ use utf8;
 
 use Carp qw( croak carp );
 use Perl::Critic::ProfileCompiler::Util qw( create_bundle );
+use Path::Tiny qw(path);
 
 ## no critic (ErrorHandling::RequireUseOfExceptions)
 my $bundle = create_bundle('Example::Author::KENTNL');
@@ -26,31 +27,31 @@ for my $wordlist (@stopwords) {
   $bundle->add_or_append_policy_field( 'Documentation::PodSpelling' => ( 'stop_words' => $wordlist ) );
 }
 
-$bundle->remove_policy('CodeLayout::RequireUseUTF8');
-$bundle->remove_policy('NamingConventions::Capitalization');
-
-#$bundle->remove_policy('Subroutines::ProhibitCallsToUnexportedSubs');
-$bundle->remove_policy('Subroutines::ProhibitSubroutinePrototypes');
-
-#$bundle->remove_policy('Subroutines::ProhibitExcessComplexity');
-
-$bundle->remove_policy('Subroutines::RequireArgUnpacking');
-
 #$bundle->remove_policy('ErrorHandling::RequireCarping');
+#$bundle->remove_policy('Subroutines::ProhibitCallsToUnexportedSubs');
+#$bundle->remove_policy('Subroutines::ProhibitExcessComplexity');
+$bundle->remove_policy('CodeLayout::RequireUseUTF8');
+$bundle->remove_policy('ErrorHandling::RequireUseOfExceptions');
 $bundle->remove_policy('NamingConventions::Capitalization');
+$bundle->remove_policy('NamingConventions::Capitalization');
+$bundle->remove_policy('Subroutines::ProhibitSubroutinePrototypes');
+$bundle->remove_policy('Subroutines::RequireArgUnpacking');
 
 my $inf = $bundle->actionlist->get_inflated;
 
 my $config = $inf->apply_config;
 
 {
-  open my $rcfile, '>', './perlcritic.rc' or croak 'Cant open perlcritic.rc';
+  my $rcfile = path('./perlcritic.rc')->openw_utf8;
   $rcfile->print( $config->as_ini, "\n" );
   close $rcfile or croak 'Something fubared closing perlcritic.rc';
 }
 my $deps = $inf->own_deps;
 {
-  open my $depsfile, '>', './perlcritic.deps' or croak 'Cant open perlcritic.deps';
+  my $target = path('./misc');
+  $target->mkpath if not $target->is_dir;
+
+  my $depsfile = $target->child('perlcritic.deps')->openw_utf8;
   for my $key ( sort keys %{$deps} ) {
     $depsfile->printf( "%s~%s\n", $key, $deps->{$key} );
     *STDERR->printf( "%s => %s\n", $key, $deps->{$key} );
